be936aef538839e1cd8a6ece1b417dfcd0bed626
jy_songxinzhang <Song756650>
2021-12-20 17:52:18 +0800

    1234

---
+++ eureka-ribbon-instance/src/main/java/eureka/ribbon/instance/EurekaRibbonInstanceApplication.java
@@ -0,0 +1,13 @@
+package eureka.ribbon.instance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRibbonInstanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRibbonInstanceApplication.class, args);
+    }
+
+}
