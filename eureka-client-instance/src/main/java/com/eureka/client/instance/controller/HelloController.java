64e6b3e9df0ef2f0af322ce06db2430f873e8e90
jy_songxinzhang <Song756650>
2021-12-20 17:43:57 +0800

    1234

--- eureka-client-instance/src/main/java/com/eureka/client/instance/controller/HelloController.java
+++ eureka-client-instance/src/main/java/com/eureka/client/instance/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.eureka.server.eurelainstance.controller;
+package com.eureka.client.instance.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
