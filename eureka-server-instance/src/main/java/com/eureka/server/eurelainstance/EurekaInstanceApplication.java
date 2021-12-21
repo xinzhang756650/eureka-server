f6c891ad2a11ec42d595e075899d8de22685b416
xinzhang756650@163.com <song756650>
2021-12-21 20:40:55 +0800

    update

--- eureka-server-instance/src/main/java/com/eureka/server/eurelainstance/EurekaInstanceApplication.java
+++ eureka-server-instance/src/main/java/com/eureka/server/eurelainstance/EurekaInstanceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurelaInstanceApplication {
+public class EurekaInstanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurelaInstanceApplication.class, args);
+        SpringApplication.run(EurekaInstanceApplication.class, args);
     }
 
 }
